906349615891e77ad69e286236efcb7439fe5f11
unknown <gabrielalcantaratp@gmail.com>
2023-05-16 01:25:20 -0300

    feat: drop items on current sqm by table of ids

---
+++ drop-items-on-current-sqm.lua
@@ -0,0 +1,16 @@
+local data = { -- Table of IDs
+  37022,
+  37027,
+  37028
+}
+
+macro(500, "Drop Items", function()
+  for _, container in pairs(getContainers()) do
+    for i, item in pairs(container:getItems()) do
+        local info = table.find(data, item:getId())
+        if info and item:getCount() == 100 then
+          return g_game.move(item, player:getPosition(), item:getCount())
+          end
+      end
+    end
+end)
